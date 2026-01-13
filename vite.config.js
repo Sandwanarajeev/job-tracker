5205e9f7f0436b5551041c9a76eb309f3bc73219
Sandwana K <sandwanarajeev@gmail.com>
2026-01-13 23:24:39 +0530

    fix base path for github pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/job-tracker/',
+  base: "/job-tracker/"   // MUST match your repo name
 })
 
